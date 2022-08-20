49914899641143578440a5f85813e7d44f895644
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-08-20 18:48:58 +0200

    creating a onSumbit function

--- src/App.js
+++ src/App.js
@@ -25,6 +25,11 @@ function App() {
     }
 ])
 
+// Add Task
+const addTask = (task) => {
+  console.log(task);
+}
+
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id));
@@ -38,7 +43,7 @@ const toggleReminder = (id) => {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? 
       (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>)
       : ('No Tasks to show')
